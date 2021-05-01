b85f8f78158c90826a8d0d2555c0d142488e283d
mlongano <mauro.longano@marconirovereto.it>
2021-05-01 21:17:01 +0200

    Add tags pages

--- components/Tags.vue
+++ components/Tags.vue
@@ -5,7 +5,7 @@
         class="podcast-tag"
         v-for="tag in post.tags"
         :key="tag.id"
-        :to='"/tag/"+tag.name'
+        :to='"/tags/"+tag.name'
       >
         <span
           class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
